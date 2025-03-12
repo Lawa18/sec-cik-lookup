7de7621cb18dd7478250bb56e5efd1d0e2a963c0
Lawa18 <95441357+Lawa18@users.noreply.github.com>
2025-03-11 22:26:44 -0400

    Update xbrl_parser.py

--- xbrl_parser.py
+++ xbrl_parser.py
@@ -1,4 +1,26 @@
 import requests
+import json
+
+def find_xbrl_url(index_url):
+    """Finds the XBRL file URL from an SEC index.json."""
+    headers = {"User-Agent": "Lars Wallin lars.e.wallin@gmail.com"}
+    response = requests.get(index_url, headers=headers)
+
+    if response.status_code != 200:
+        return None
+
+    try:
+        data = response.json()
+        if "directory" in data and "item" in data["directory"]:
+            for file in data["directory"]["item"]:
+                if file["name"].endswith(".xml") and "htm.xml" in file["name"]:
+                    return index_url.replace("index.json", file["name"])
+    except json.JSONDecodeError:
+        return None
+
+    return None  # No XBRL file found
+
+import requests
 from lxml import etree
 
 def extract_summary(xbrl_url):
